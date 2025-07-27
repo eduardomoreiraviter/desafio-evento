6bf5b266f0f83070ca1b76756ced93ffc232204b
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-07-27 02:55:34 -0300

    first commit

---
+++ desafiodemo/src/main/java/com/viter/desafiodemo/DesafiodemoApplication.java
@@ -0,0 +1,13 @@
+package com.viter.desafiodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodemoApplication.class, args);
+	}
+
+}
